b95cfb9d8a058e7cf4081265822582cfe33a5bc6
Manuel Alvarez Sanchez <malvasan@hotmail.com>
2025-11-19 01:57:50 -0500

    chore: status of the response added

--- app.js
+++ app.js
@@ -9,15 +9,19 @@ const server = http.createServer((req, res)=>{
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         case '/contact-me':
             path += 'contact-me.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 

39c8f5f777d0e4cd6661ff319d0beefd8888f281
Manuel Alvarez Sanchez <malvasan@hotmail.com>
2025-11-19 12:07:16 -0500

    chore: using express for a basic site

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
-import http from 'http';
-import fs from 'fs';
+// import http from 'http';
+// import fs from 'fs';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from "path";
 
-const server = http.createServer((req, res)=>{
-    res.setHeader('Content-Type', 'text/html');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    let path = './views/';
+const app = express();
 
-    switch (req.url) {
-        case '/':
-            path += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        case '/contact-me':
-            path += 'contact-me.html';
-            res.statusCode = 200;
-            break;
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break;
+app.get('/',(req,res)=>{
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about',(req,res)=>{
+    res.sendFile('./views/about.html', {root: __dirname});
+});
+
+app.get('/contact-me',(req,res)=>{
+    res.sendFile('./views/contact-me.html', {root: __dirname});
+});
+
+app.get('/about-me',(req,res)=>{
+    res.redirect('/about');
+});
+
+app.use((req, res)=>{
+    res.sendFile('./views/404.html', {root: __dirname});
+})
+
+app.listen(3000, (error)=>{
+    if (error) {
+        throw error;
     }
-
-    fs.readFile(path,(err,data)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.end(data);
-        }
-    })
+    console.log('My first Express app');
 })
 
-server.listen(3000, 'localhost',()=>{
-    console.log('listening for requests on port 3000');
-})
+// const server = http.createServer((req, res)=>{
+//     res.setHeader('Content-Type', 'text/html');
+
+//     let path = './views/';
+
+//     switch (req.url) {
+//         case '/':
+//             path += 'index.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about':
+//             path += 'about.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/contact-me':
+//             path += 'contact-me.html';
+//             res.statusCode = 200;
+//             break;
+//         default:
+//             path += '404.html';
+//             res.statusCode = 404;
+//             break;
+//     }
+
+//     fs.readFile(path,(err,data)=>{
+//         if(err){
+//             console.log(err)
+//         }else{
+//             res.end(data);
+//         }
+//     })
+// })
+
+// server.listen(3000, 'localhost',()=>{
+//     console.log('listening for requests on port 3000');
+// })
